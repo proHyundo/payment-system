ca3c9579d5dd161accd7f75ab7c9e62a8267c9b8
proHyundo <guseh08@knou.ac.kr>
2025-06-17 20:58:18 +0900

    feat(entity): 베이스 엔티티 추가

---
+++ src/main/java/pro/hyundo/paymentsystem/adapter/out/persistence/entity/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package pro.hyundo.paymentsystem.adapter.out.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * 생성/수정 시간을 자동으로 관리하는 Base Entity
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
