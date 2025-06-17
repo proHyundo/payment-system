58d7e7ea98bdb98dea4e89016b9d65946ceb5e39
proHyundo <guseh08@knou.ac.kr>
2025-06-17 20:10:46 +0900

    feat(annotation): 웹 어댑터임을 나타내는 커스텀 어노테이션 추가

---
+++ src/main/java/pro/hyundo/paymentservice/global/annotation/WebAdapter.java
@@ -0,0 +1,20 @@
+package pro.hyundo.paymentservice.global.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+/**
+ * 웹 어댑터임을 나타내는 커스텀 어노테이션입니다.
+ * 이 어노테이션이 붙은 클래스는 Spring의 Component로 스캔됩니다.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface WebAdapter {
+
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+}
